e2e56197f47b8642cce01bf778b939e1bd6845bd
Ge Chunjiang <41175708+John-Ge@users.noreply.github.com>
2021-02-04 23:04:45 +0800

    Update tricks.py

--- tricks.py
+++ tricks.py
@@ -1,19 +1,5 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-## convolution a by w
-def conv(a, w):
-    a = torch.randn(3, 5, 10, 10).cuda()
-    w = torch.randn(7, 5, 3, 3).cuda()
-    res1 = torch.nn.functional.conv2d(a, w, padding=1)
-    afold = torch.nn.functional.unfold(a, 3, 1, 1)
-    # batch, c, h, w -> batch, c*k*k, n. n is the feature left 
-    wfold = w.view((w.size(0), -1))
-    res2 = torch.matmul(wfold, afold).reshape(a.size(0), w.size(0), a.size(2), a.size(3))
-    
-Some tricks could make pytorch more quickly
-
+# Some tricks could make pytorch more quickly
+"""
 IO
 
 - use opencv as the dataloader
@@ -29,8 +15,44 @@ CODE
 - DistributedDataParallel instead of DataParallel
 - set shuffle = False when eval and test
 - when computational graph does not change, set torch.backends.cudnn.benchmark = True
+- try to use torch.ops() rather than tensor.ops()
+- transpose() rather than t() and permuter
 
 TRAIN
 
 - optimizer: Adamw
 - mixed precision trained
+"""
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+## convolution a by w
+def conv(a, w):
+    a = torch.randn(3, 5, 10, 10).cuda()
+    w = torch.randn(7, 5, 3, 3).cuda()
+    res1 = torch.nn.functional.conv2d(a, w, padding=1)
+    afold = torch.nn.functional.unfold(a, 3, 1, 1)
+    # batch, c, h, w -> batch, c*k*k, n. n is the feature left 
+    wfold = w.view((w.size(0), -1))
+    res2 = torch.matmul(wfold, afold).reshape(a.size(0), w.size(0), a.size(2), a.size(3))
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
